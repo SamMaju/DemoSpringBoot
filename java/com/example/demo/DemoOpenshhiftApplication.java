319cfac0a3e1aee7c7e9c251e7e53bcdb4d9b917
SamMaju <47913640+SamMaju@users.noreply.github.com>
2019-02-23 14:04:41 +0530

    Add files via upload

---
+++ java/com/example/demo/DemoOpenshhiftApplication.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoOpenshhiftApplication {
+
+	@GetMapping("/welocme")
+	public String getWelcome() {
+		return "Welcom to java open Shift.";
+		
+	}
+	
+	@GetMapping("/{input}")
+	public String congrats(@PathVariable String input) {
+		return "Congratulation " +input + " your Openshift successfuly deployed!!";
+		
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(DemoOpenshhiftApplication.class, args);
+		System.out.println("Inside");
+	}
+
+}
